9cc18e99dd78b29d10a8558aa2d55faad58cf9de
Joseph Walker <josephwalker@tred.com>
2018-11-16 10:31:52 -0600

    100% Maybe Coverage

--- tests/algebra/maybe.spec.ts
+++ tests/algebra/maybe.spec.ts
@@ -2,10 +2,12 @@ import 'mocha';
 import { expect } from 'chai';
 
 import { Maybe } from '../../src/algebra/maybe';
+import { Either } from '../../src/algebra/either';
+import { Response } from '../../src/algebra/response';
 import { functorLaws, applicativeLaws, monadLaws } from './typeclass.spec';
 import { testLiftN } from './lift.spec';
 
-function expectEquality<T>(valueA: T, valueB: T) {
+function expectDeepEquality<T>(valueA: T, valueB: T) {
 	expect(valueA).to.be.deep.equal(valueB);
 }
 
@@ -31,43 +33,84 @@ describe('Maybe Monad', function() {
 	describe('Maybe Creation', function() {
 		describe('from()', function() {
 			it('should create a Maybe from a nullable or undefined value', function() {
-				expectEquality(Maybe.from(true), Maybe.Just(true));
-				expectEquality(Maybe.from(false), Maybe.Just(false));
-				expectEquality(Maybe.from(null), Maybe.Nothing());
-				expectEquality(Maybe.from(undefined), Maybe.Nothing());
+				expectDeepEquality(Maybe.from(true), Maybe.Just(true));
+				expectDeepEquality(Maybe.from(false), Maybe.Just(false));
+				expectDeepEquality(Maybe.from(null), Maybe.Nothing());
+				expectDeepEquality(Maybe.from(undefined), Maybe.Nothing());
 			});
 		});
 
 		describe('fromNullable()', function() {
 			it('should create a Maybe from a nullable value, passing through Undefined', function() {
-				expectEquality(Maybe.fromNullable(null), Maybe.Nothing());
-				expectEquality(Maybe.fromNullable(false), Maybe.Just(false));
-				expectEquality(Maybe.fromNullable(undefined), Maybe.Just(undefined));
+				expectDeepEquality(Maybe.fromNullable(null), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromNullable(false), Maybe.Just(false));
+				expectDeepEquality(Maybe.fromNullable(undefined), Maybe.Just(undefined));
 			});
 		});
 
 		describe('fromUndefined()', function() {
 			it('should create a Maybe from a defined value, passing through null', function() {
-				expectEquality(Maybe.fromUndefined(null), Maybe.Just(null));
-				expectEquality(Maybe.fromUndefined(false), Maybe.Just(false));
-				expectEquality(Maybe.fromUndefined(undefined), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromUndefined(null), Maybe.Just(null));
+				expectDeepEquality(Maybe.fromUndefined(false), Maybe.Just(false));
+				expectDeepEquality(Maybe.fromUndefined(undefined), Maybe.Nothing());
 			});
 		});
 
 		describe('fromTruthy()', function() {
 			it('should create a Maybe from a truthy value', function() {
-				expectEquality(Maybe.fromTruthy(true), Maybe.Just(true));
-				expectEquality(Maybe.fromTruthy(false), Maybe.Nothing());
-				expectEquality(Maybe.fromTruthy(null), Maybe.Nothing());
-				expectEquality(Maybe.fromTruthy(undefined), Maybe.Nothing());
-				expectEquality(Maybe.fromTruthy(0), Maybe.Nothing());
-				expectEquality(Maybe.fromTruthy(1), Maybe.Just(1));
+				expectDeepEquality(Maybe.fromTruthy(true), Maybe.Just(true));
+				expectDeepEquality(Maybe.fromTruthy(false), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromTruthy(null), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromTruthy(undefined), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromTruthy(0), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromTruthy(1), Maybe.Just(1));
 			});
 		});
 	});
 
 	describe('Maybe Casting', function() {
+		describe('fromEither()', function() {
+			it('should create a Maybe from an Either', function() {
+				const left = Either.Left(1);
+				const right = Either.Right(2);
 
+				expectDeepEquality(Maybe.fromEither(left), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromEither(right), Maybe.Just(2));
+			});
+		});
+
+		describe('fromResponse()', function() {
+			const loading = Response.Loading();
+			const error = Response.Error(1);
+			const ready = Response.Ready(2);
+
+			it('should create a Maybe from a Response', function() {
+				expectDeepEquality(Maybe.fromResponse(loading), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromResponse(error), Maybe.Nothing());
+				expectDeepEquality(Maybe.fromResponse(ready), Maybe.Just(2));
+			});
+		});
+
+		describe('toEither()', function() {
+			it('should turn a Maybe into an Either', function() {
+				expectDeepEquality(just.toEither(-1), Either.Right(4));
+				expectDeepEquality(nothing.toEither(-1), Either.Left(-1));
+			});
+		});
+
+		describe('toLoadingResponse()', function() {
+			it('should turn a Maybe into a Response, with Nothing becoming Loading', function() {
+				expectDeepEquality(just.toLoadingResponse(), Response.Ready(4));
+				expectDeepEquality(nothing.toLoadingResponse(), Response.Loading());
+			});
+		});
+
+		describe('toErrorResponse()', function() {
+			it('should turn a Maybe into a Response, with Nothing becoming Error', function() {
+				expectDeepEquality(just.toErrorResponse(-1), Response.Ready(4));
+				expectDeepEquality(nothing.toErrorResponse(-1), Response.Error(-1));
+			});
+		});
 	});
 
 	describe('Maybe Class Methods', function() {
@@ -84,14 +127,29 @@ describe('Maybe Monad', function() {
 				const maybeAddOne = (n: number) => Maybe.Just(n + 1);
 				const ns = [1, 2, 3];
 
-				expect(Maybe.traverse(maybeAddOne, ns)).to.be.deep.equal(Maybe.Just([2, 3, 4]));
+				expectDeepEquality(Maybe.traverse(maybeAddOne, ns), Maybe.Just([2, 3, 4]));
 			});
 
 			it('should handle the null case of the Maybe-returning function returning Nothing', function() {
 				const maybeAddOne = (n: number) => n === 2 ? Maybe.Nothing() : Maybe.Just(n + 1);
 				const ns = [1, 2, 3];
 
-				expect(Maybe.traverse(maybeAddOne, ns)).to.be.deep.equal(Maybe.Nothing());
+				expectDeepEquality(Maybe.traverse(maybeAddOne, ns), Maybe.Nothing());
+			});
+		});
+
+		describe('sequence()', function() {
+			it('should flatten a list of Maybes into a Maybe list', function() {
+				const maybeList = [
+					Maybe.Just(1),
+					Maybe.Just(2),
+					Maybe.Just(3)
+				];
+
+				const nothingList = maybeList.concat([Maybe.Nothing()]);
+
+				expectDeepEquality(Maybe.sequence(maybeList), Maybe.Just([1, 2, 3]));
+				expectDeepEquality(Maybe.sequence(nothingList), Maybe.Nothing());
 			});
 		});
 	});
@@ -128,7 +186,7 @@ describe('Maybe Monad', function() {
 		describe('extractUnsafe()', function() {
 			it('should return a Just value, or throw if the Maybe is Nothing', function() {
 				expect(just.extractUnsafe()).to.be.equal(4);
-				expect(() => nothing.extractUnsafe()).to.throw();
+				expect(() => nothing.extractUnsafe()).to.throw('Type Constraint Failure');
 			});
 		});
 
